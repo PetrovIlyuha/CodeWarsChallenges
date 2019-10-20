eb4bafdaf8b4095d8d823b7d969443aa172d94ec
Ilya Petrov <dnamix1@gmail.com>
2019-10-20 08:31:14 +0300

    slightly change to longest K strings consequtive still Code Wars giving errors(

--- longestKString.js
+++ longestKString.js
@@ -1,12 +1,39 @@
 function longestConsec(strarr, k) {
-  let longestConcatKstrings = [];
-  for (let i = 0; i <= strarr.length; i++) {
-     let longestConcatKstrings[i] = [];
-    for (let j = 0; j <= k; j++) {
-         longestConcatKstrings[i].push(strarr[j]);   
+  let longestConcatKstrings = Array(strarr.length - 1).fill('')   
+  for (let i = 0; i <= strarr.length - k; i++) {
+    for (let j = i; j < k + i; j++) {
+        longestConcatKstrings[i] += strarr[j]
+      }     
+    }      
+    let longest;
+    if ( k > 1) {
+      for (let i = 0; i <= longestConcatKstrings.length - 2; i++) {
+        if (longestConcatKstrings[i].length < longestConcatKstrings[i+1].length) {
+          longest = longestConcatKstrings[i+1];
+         }
+       }
+    } else {
+     longest = strarr.sort((a,b) => b.length - a.length)[0];
     }
-  }
-  return longestConcatKstrings;
-}
+   return longest;
+ }
+
+
+//  function longestConsec(strarr, k) {
+//   let longestConcatKstrings = Array(strarr.length - 1).fill('')   
+//    for (let i = 0; i <= strarr.length - k; i++) {
+//      for (let j = i; j < k + i; j++) {
+//          longestConcatKstrings[i] += strarr[j]
+//        }     
+//      }      
+//      let longest;     
+//      if (k > 1) {
+//        longest = longestConcatKstrings.sort((a,b) =>  b.length - a.length)[0];
+//      } else {
+//        longest = strarr.sort((a,b) => b.length - a.length)[0];
+//      }
+//      return longest;
+//  }
 
 console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2))
+console.log(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1))
