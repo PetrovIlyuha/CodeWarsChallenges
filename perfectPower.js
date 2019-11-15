62e21f470ab004e4a2ad17fbbe4d8788a243dc1b
Ilya Petrov <dnamix1@gmail.com>
2019-11-15 09:58:51 +0300

    task on perfect power of number

--- perfectPower.js
+++ perfectPower.js
@@ -1,11 +1,11 @@
 var isPP = function(n) {
   let perfPowers = [];
-  for (let i = 1; i < 110; i += 1) {    
-    for (let j = 2; j < 110; j += 1) {      
-      if (i ** j === n) perfPowers.push([i,j]);      
-    } else { return null}
+  for (let i = 1; i < 110; i += 1) {
+    for (let j = 2; j < 110; j += 1) {
+      if (i ** j === n) perfPowers.push([i, j]);
+    }
   }
-  return perfPowers;
+  return perfPowers.length ? perfPowers : null;
 };
 
 console.log(isPP(81));
